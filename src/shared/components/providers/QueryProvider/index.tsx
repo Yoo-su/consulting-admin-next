c1aaba15d5b3dab0527aa130581eff5bb30f0ff7
유수현 <rhan0871@naver.com>
2024-04-30 17:19:09 +0900

    feat: tanstack setting

---
+++ src/shared/components/providers/QueryProvider/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import { ReactNode, useState } from "react";
+
+type Props = {
+  children: ReactNode;
+}
+const QueryProvider = ({ children }: Props) => {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60, // 1분
+      }
+    }
+  }));
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
