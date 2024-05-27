5b3794fb8ab3c5c24797c2042e1382504a36a9ab
jhk88 <deftones88@naver.com>
2024-05-27 17:52:55 +0900

    pwa 주소 화면 구성

--- src/features/dashboard/hooks/tanstack/use-get-app-version-history-query.tsx
+++ src/features/dashboard/hooks/tanstack/use-get-app-version-history-query.tsx
@@ -3,11 +3,11 @@
 import { useQuery, skipToken } from '@tanstack/react-query';
 import { getAppVersionHistory } from '../../apis/get-app-version-history';
 
-export const useGetAppVersionHistoryQuery = (serviceID: string | undefined, osType: 'P' | 'A' | null) => {
+export const useGetAppVersionHistoryQuery = (serviceID: string | undefined, osType: 'O' | 'P' | 'A' | null) => {
   return useQuery({
     queryKey: ['get-app-version-history', serviceID],
-    queryFn: serviceID && osType ? () => getAppVersionHistory(serviceID, osType) : skipToken,
-    enabled: !!(serviceID && osType),
+    queryFn: serviceID && (osType == 'P' || osType == 'A') ? () => getAppVersionHistory(serviceID, osType) : skipToken,
+    enabled: !!(serviceID && (osType == 'P' || osType == 'A')),
     refetchOnMount: true,
     staleTime: 0,
   });
