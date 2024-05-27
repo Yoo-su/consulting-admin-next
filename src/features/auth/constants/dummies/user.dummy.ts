51f5dcf682451cbcf0b9432318f437d67669f44d
yoosu <rhan0871@naver.com>
2024-05-27 10:20:39 +0900

    feat: user dummy 제거

--- src/features/auth/constants/dummies/user.dummy.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../../types/user.type';
-
-export const dummyUser: User = {
-  userID: 'chess',
-  name: 'Kafka',
-  role: 'developer',
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiJ5b29zdSJ9.59LuJ69FHlGjREbbaB9xiXNudt7XY-nkt-HzG3gqMkM',
-};
