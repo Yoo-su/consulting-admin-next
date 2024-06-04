7b8eb33da842ba118874e99a7bf2af9b7f70826c
chess-jinhak <chess@jinhakapply.com>
2024-06-04 17:18:43 +0900

    feat: AdminGroup 타입 추가

--- src/features/auth/types/user.type.ts
+++ src/features/auth/types/user.type.ts
@@ -1,7 +1,14 @@
+export enum AdminGroup {
+  Manager = 1,
+  Developer = 2,
+  ConsultingAdminDeveloper = 3,
+}
+
 export type User = {
   sub: string;
   userName: string;
   departmentID: 1 | 2;
+  groupIdList: AdminGroup[];
   iat: number;
   exp: number;
   profileImage?: string;
