bb30e11701a592945208de4a241c6db7171b0b42
chess-jinhak <chess@jinhakapply.com>
2024-06-10 11:31:19 +0900

    refactor: Refactor sheet name conversion logic

--- src/features/dashboard/hooks/use-handle-excel.tsx
+++ src/features/dashboard/hooks/use-handle-excel.tsx
@@ -101,6 +101,21 @@ export const useHandleExcel = () => {
     return await readExcelFile(excel);
   };
 
+  const sheetNameMap: Record<string, string> = {
+    Service: '서비스정보',
+    Code: '코드표',
+    Seltype: '전형상담타입',
+    College: '단과대학',
+    Order: '계열',
+    Major: '학과설명',
+    ConstraintCase: '수능제약케이스',
+    SATCase: '수능점수케이스',
+    SATSelect: '수능선택과목',
+    HSBCase: '학생부점수케이스',
+    HSBInput: '학생부점수케이스입력',
+    Schedule: '입시일정',
+  };
+  
   /**
    * 시트명 한-영 전환 메서드
    * @param str 변환을 원하는 문자열
@@ -108,38 +123,13 @@ export const useHandleExcel = () => {
    * @returns
    */
   const convertSheetName = (str: string, lang: 'eng' | 'kor') => {
-    let returnStr = '';
-    if (lang == 'eng') {
-      if (str == 'Service') returnStr = '서비스정보';
-      else if (str == 'Code') returnStr = '코드표';
-      else if (str == 'Seltype') returnStr = '전형상담타입';
-      else if (str == 'College') returnStr = '단과대학';
-      else if (str == 'Order') returnStr = '계열';
-      else if (str == 'Major') returnStr = '학과설명';
-      else if (str == 'ConstraintCase') returnStr = '수능제약케이스';
-      else if (str == 'SATCase') returnStr = '수능점수케이스';
-      else if (str == 'SATSelect') returnStr = '수능선택과목';
-      else if (str == 'HSBCase') returnStr = '학생부점수케이스';
-      else if (str == 'HSBInput') returnStr = '학생부점수케이스입력';
-      else if (str == 'Schedule') returnStr = '입시일정';
-      else returnStr = str;
-      return returnStr;
-    } else if (lang == 'kor') {
-      if (str == '서비스정보') returnStr = 'Service';
-      else if (str == '코드표') returnStr = 'Code';
-      else if (str == '전형상담타입') returnStr = 'Seltype';
-      else if (str == '단과대학') returnStr = 'College';
-      else if (str == '계열') returnStr = 'Order';
-      else if (str == '학과설명') returnStr = 'Major';
-      else if (str == '수능제약케이스') returnStr = 'ConstraintCase';
-      else if (str == '수능점수케이스') returnStr = 'SATCase';
-      else if (str == '수능선택과목') returnStr = 'SATSelect';
-      else if (str == '학생부점수케이스') returnStr = 'HSBCase';
-      else if (str == '학생부점수케이스입력') returnStr = 'HSBInput';
-      else if (str == '입시일정') returnStr = 'Schedule';
-      else returnStr = str;
+    if (lang === 'eng') {
+      return sheetNameMap[str] || str;
+    } else if (lang === 'kor') {
+      const korName = Object.keys(sheetNameMap).find((key) => sheetNameMap[key] === str);
+      return korName || str;
     }
-    return returnStr;
+    return str;
   };
 
   /**
