9d0ea65278e7af909933667878e44ff09f7a2993
chess-jinhak <chess@jinhakapply.com>
2024-06-10 12:13:58 +0900

    refactor: Remove unnecessary webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.minimize = false;
-    }
-    config.optimization.splitChunks = {
-      cacheGroups: {
-        default: false,
-      },
-    };
-    config.optimization.runtimeChunk = false;
-    return config;
-  },
   async redirects() {
     return [
       // Basic redirect
