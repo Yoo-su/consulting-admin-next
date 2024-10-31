bf122ebba6f7cf4b1e328ab58c9dc0f798f8dcac
jhk88 <deftones88@naver.com>
2024-10-31 16:26:02 +0900

    hotfix: HOTFIXXXXXXXXXXXXX

--- src/features/overview/hooks/use-get-consultingapp-state-all-query.tsx
+++ src/features/overview/hooks/use-get-consultingapp-state-all-query.tsx
@@ -1,11 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getConsultingAppStateAll } from '../apis';
+import { useUser } from '@/shared/hooks/context';
 
 export const useGetConsultingAppStateAllQuery = () => {
+  const { isAdmin } = useUser();
+
   return useQuery({
     queryKey: ['consultingAppStateAll'],
     queryFn: () => getConsultingAppStateAll(),
     refetchOnMount: true,
     refetchOnWindowFocus: true,
+    enabled: isAdmin,
   });
 };
