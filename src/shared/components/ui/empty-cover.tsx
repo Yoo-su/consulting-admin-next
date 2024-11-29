dc7c20f518b56c835002722f6e64a7c15c2213ac
jhk88 <deftones88@naver.com>
2024-11-29 12:10:42 +0900

    feat: more img files for emptycover

--- src/shared/components/ui/empty-cover.tsx
+++ src/shared/components/ui/empty-cover.tsx
@@ -1,13 +1,41 @@
 import { Box, SxProps, Typography } from '@mui/material';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
+import candyCane from '@/shared/assets/svgs/candyCane.svg';
 import christmasDecorationDoor from '@/shared/assets/svgs/christmasDecorationDoor.svg';
+import hotChocoMug from '@/shared/assets/svgs/hotChocoMug.svg';
+import snowSleigh from '@/shared/assets/svgs/snowSleigh.svg';
 
 type EmptyCoverProps = {
   message: string;
   sx?: SxProps;
 };
 const EmptyCover = ({ message, sx }: EmptyCoverProps) => {
+  const [selectedImage, setSelectedImage] = useState(0);
+  const images = [
+    {
+      src: christmasDecorationDoor,
+      alt: 'christmasDecorationDoor',
+    },
+    {
+      src: snowSleigh,
+      alt: 'snowSleigh',
+    },
+    {
+      src: hotChocoMug,
+      alt: 'hotChocoMug',
+    },
+    {
+      src: candyCane,
+      alt: 'candyCane',
+    },
+  ];
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setSelectedImage(randomIndex);
+  }, []);
   return (
     <Box
       sx={{
@@ -25,8 +53,8 @@ const EmptyCover = ({ message, sx }: EmptyCoverProps) => {
       }}
     >
       <Image
-        src={christmasDecorationDoor}
-        alt={'christmasDecorationDoor'}
+        src={images[selectedImage].src}
+        alt={images[selectedImage].alt}
         width={64}
         height={64}
       />
