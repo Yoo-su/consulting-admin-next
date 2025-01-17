f1323d6f5d078a26732797a6956cd80868ec949a
jhk88 <deftones88@naver.com>
2025-01-17 13:39:01 +0900

    fix: emptylist tbody error

--- src/pages_fsd/version-setting/components/misc/empty-list.tsx
+++ src/pages_fsd/version-setting/components/misc/empty-list.tsx
@@ -1,15 +1,18 @@
-import { TableCell, TableRow, Typography } from '@mui/material';
+import { TableBody, TableCell, TableRow, Typography } from '@mui/material';
+import { TableBodyClass } from '../../constants';
 
 export const EmptyList = () => {
   return (
-    <TableRow sx={{ height: '200px' }}>
-      <TableCell colSpan={2}>
-        <Typography align="center">
-          데이터가 없습니다.
-          <br />
-          (기초를 업로드 해주세요.)
-        </Typography>
-      </TableCell>
-    </TableRow>
+    <TableBody sx={TableBodyClass}>
+      <TableRow sx={{ height: '200px' }}>
+        <TableCell colSpan={2}>
+          <Typography align="center">
+            데이터가 없습니다.
+            <br />
+            (기초를 업로드 해주세요.)
+          </Typography>
+        </TableCell>
+      </TableRow>
+    </TableBody>
   );
 };
